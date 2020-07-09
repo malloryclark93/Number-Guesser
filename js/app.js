b4a66ce3db508c607e2b32a2392aabc9c983cd75
malloryclark93 <mallory_clark@live.com>
2020-07-09 15:55:13 -0600

    error msg in red

--- js/app.js
+++ js/app.js
@@ -17,4 +17,27 @@ const game = document.querySelector('#game'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
       guessInput = document.querySelector('#guess-input'),
-      message = document.querySelector('.message'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max 
+      minNum.textContent = min;
+      maxNum.textContent = max;
+
+      // Listen for guess 
+      guessBtn.addEventListener('click', function(){
+        let guess = parseInt(guessInput.value);
+          console.log(guess);
+        // Validate our input
+        if(isNaN(guess) || guess < min || guess > max){
+          setMessage(`Please Enter A Number Between ${min} and ${max}`, `red`);
+
+        }
+
+      });
+
+      // Set message
+      function setMessage(msg, color){
+        message.style.color = color;
+        message.textContent = msg;
+
+      }
